f80bc0316064bb1730f81022c1d8bcd9249e64f1
Chu Hao Wen <chuhaow@gmail.com>
2022-08-17 17:11:28 -0500

    Began work on new animation script

---
+++ Assets/Scripts/ProceduralLeg.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralLeg : MonoBehaviour
+{
+    [SerializeField] private Transform ikTarget;
+    [SerializeField] private Transform ikPole;
+    [SerializeField] private Vector3 restingPosition;
+    [SerializeField] private Vector3 worldPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        worldPosition = transform.TransformPoint(restingPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateIKTarget();
+        
+    }
+    
+    private void UpdateIKTarget()
+    {
+        ikTarget.position = worldPosition;//Vector3.Lerp(ikTarget.position, restingPosition, Time.deltaTime);
+        
+    }
+
+    private void Step()
+    {
+
+    }
+}
